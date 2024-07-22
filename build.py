37aa2324ed022bbea4a9c41e8bf532bb9126e8ef
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2024-07-21 19:33:58 -0600

    Just read more instead

--- build.py
+++ build.py
@@ -47,7 +47,7 @@ class BashRunnerWithSharedEnvironment(AbstractContextManager):
         result = subprocess.run(
             ["bash", "-ce", cmd], pass_fds=[self._fd_write], env=self.env, **opts
         )
-        self.env = json.loads(os.read(self._fd_read, os.fstat(self._fd_read).st_size).decode())
+        self.env = json.loads(os.read(self._fd_read, 10000).decode())
         return result
 
     def __exit__(self, exc_type, exc_value, traceback):
